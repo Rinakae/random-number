5ce435abedd59837826b156dc532136ca78f4e19
Rinakae <simi1993@mail.ru>
2022-02-07 19:42:17 +0300

    add task 1 random number

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+const isNumber = function(n) {
+  return (!isNaN(parseInt(n)) && isFinite(n));
+};
+
+function askNumber() {
+  let randomNum = 15;
+  let num = prompt("Угадай число от 1 до 100", 12);
+
+  if (num === null) {
+    alert("Игра окончена");
+  } else if (!isNumber(num) && num !== null) {
+    alert("Введите число!");
+    askNumber();
+  } else if (num > randomNum && num !== null) {
+    alert("Загаданное число меньше");
+    askNumber();
+  } else if (num < randomNum && num !== null) {
+    alert("Загаданное число больше");
+    askNumber();
+  } else if (num == randomNum && num !== null) {
+    alert("Поздравляю, Вы угадали!");
+  }
+}
+
+askNumber();
