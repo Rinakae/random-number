99209e016922864a13bca693d78cc5ff5e0452ea
Rinakae <simi1993@mail.ru>
2022-02-08 20:14:14 +0300

    fix scope askNumber

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 'use strict';
-const isNumber = function(n) {
-  return (!isNaN(parseInt(n)) && isFinite(n));
-};
-let randomNum = Math.floor(Math.random() * 101);
-function askNumber() {
-  
-  let num = prompt("Угадай число от 1 до 100");
+const game = function() {
 
-  if (num === null) {
-    alert("Игра окончена");
-  } else if (!isNumber(num) && num !== null) {
-    alert("Введите число!");
-    askNumber();
-  } else if (num > randomNum && num !== null) {
-    alert("Загаданное число меньше");
-    askNumber();
-  } else if (num < randomNum && num !== null) {
-    alert("Загаданное число больше");
-    askNumber();
-  } else if (num == randomNum && num !== null) {
-    alert("Поздравляю, Вы угадали!");
-  }
-}
+  let isNumber = function(n) {
+    return (!isNaN(parseInt(n)) && isFinite(n));
+  };
+
+  let random = function() {
+    return Math.floor(Math.random() * 101);
+  };
+
+  let randomNumber = random();
+
+  function askNumber() {
+    let userNumber = prompt("Угадай число от 1 до 100");
+    console.dir(askNumber);
+
+    if (userNumber === null) {
+      alert("Игра окончена");
+    } else if (!isNumber(userNumber) && userNumber !== null) {
+      alert("Введите число!");
+      askNumber();
+    } else if (userNumber > randomNumber && userNumber !== null) {
+      alert("Загаданное число меньше");
+      askNumber();
+    } else if (userNumber < randomNumber && userNumber !== null) {
+      alert("Загаданное число больше");
+      askNumber();
+    } else if (userNumber == randomNumber && userNumber !== null) {
+      alert("Поздравляю, Вы угадали!");
+    }
+  } 
 
 askNumber();
+
+};
+
+game();
